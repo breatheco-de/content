adf35d71b921f1bf92a2fdbb24683187a567ae7c
Alejandro Sanchez <aalejo@gmail.com>
2018-11-07 15:15:42 +0000

    reading time and fix on the lessons url

--- src/components/cover/cover.jsx
+++ src/components/cover/cover.jsx
@@ -5,7 +5,7 @@ export const Cover = ({title, subtitle, time}) => (<div className={styles.cover}
     <div className="container">
         <div className="row">
             <div className="col">
-                <small>Reading time: {time}</small>
+                <small>{time}</small>
                 <h1>{title}</h1>
             </div>
             <div className="col mt-4">
