0b2633d1f68fb19024c280fae79b4daf77728e4e
Rafael Esaa <esaarafael@hotmail.com>
2018-12-11 15:04:21 -0400

    se configuro el plugin de google para que funcionara en el ambiente de desarrollo y se modifico un markdown de how-to/

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,14 +117,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
         id: "GTM-574Z6C5",
-  
-        // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-  
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+        includeInDevelopment: true,
       },
     }
   ],
