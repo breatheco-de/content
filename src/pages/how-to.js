f087accd0707e313d2e7aa53a06923a0f701c9b6
Alejandro Sanchez <aalejo@gmail.com>
2019-02-26 01:17:43 +0000

    index for how-to and errors

---
+++ src/pages/how-to.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import { graphql } from 'gatsby';
+
+export default ({ data }) => {
+  const post = data.allMarkdownRemark.edges.map(({node}, key)=>{
+    console.log(node.fields);
+    if(node.fields.type === "how-to"){
+      return (
+        <div className="col-md-4" key={key}>
+          <Link to={node.fields.url}>
+            <h3>{node.frontmatter.title}{" "}</h3>
+          </Link>
+            <p>{node.excerpt}</p>
+        </div>
+      );
+    }
+    return '';
+  });
+
+  return (
+      <div>
+        <h1>Publicaciones:</h1>
+        <div className="row">
+          { post }
+        </div>
+      </div>
+  );
+};
+
+export const query = graphql`
+  query MyQueryHowTo{
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+            url
+            type
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
