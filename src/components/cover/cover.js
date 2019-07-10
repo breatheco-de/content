6877407e5452d10338c93e7cb9518b0e82a9a790
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-07-10 15:55:29 +0000

    completed Tooltip and fixed issues

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './cover.module.scss';
+import Popover from "../popover/Popover";
+import PropTypes from "prop-types";
+import { GithubCard, Button, Loading } from "@breathecode/ui-components";
 
 export const Cover = ({title, subtitle, time, background, textColor, status, authors}) => (<div
     style={{
@@ -18,7 +21,7 @@ export const Cover = ({title, subtitle, time, background, textColor, status, aut
                 <h1>{title}</h1>
                 { Array.isArray(authors) &&
                     <span>
-                        by {authors.map((a,i)=> (<a key={i} className="author badge badge-secondary mr-2" rel="noopener noreferrer nofollow" target="_blank" href={"https://github.com/"+a}>@{a}</a>))}
+                        by {authors.map((a,i)=> (<a key={i} className="author badge badge-secondary mr-2" rel="noopener noreferrer nofollow" target="_blank" href={"https://github.com/"+a}>@{<Popover body={<GithubCard gitUsername={a} />}>{a}</Popover>}</a>))}
                     </span>
                 }
             </div>
@@ -28,3 +31,7 @@ export const Cover = ({title, subtitle, time, background, textColor, status, aut
         </div>
     </div>
 </div>);
+Cover.propTypes = {
+  history: PropTypes.object,
+  match: PropTypes.object
+};
